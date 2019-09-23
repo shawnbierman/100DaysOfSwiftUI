cee2993a42069202ffcb433fd0233940c96b16b1
Shawn Bierman <shawn.bierman@mlh.org>
2019-09-23 11:14:09 -0500

    Day 1: Strings, Ints, Type Inference, Type Annotations

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+var age = 38
+var population = 8_000_000
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+// without new lines
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var pi = 3.141      // double
+var awesome = true  // boolean
+
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+let taylor = "swift" // constant
+
+let str4 = "Hello, Playground" // type inference
+let album: String = "Reputation"  // type annotations...
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
